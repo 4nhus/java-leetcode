1d12ae572357e6b0dfc8bd8382602e93611c5198
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-04-10 22:14:32 +1000

    Complete Grind75 Java fast and slow pointers

---
+++ src/FastAndSlowPointers.java
@@ -0,0 +1,38 @@
+public class FastAndSlowPointers {
+    class LinkedListNode {
+        public int data;
+        public LinkedListNode next;
+        // Constructor will be used to make a LinkedListNode type object
+        public LinkedListNode(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static boolean detectCycle(LinkedListNode head) {
+        LinkedListNode slow = head;
+        LinkedListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static LinkedListNode middleNode(LinkedListNode head) {
+        LinkedListNode slow = head;
+        LinkedListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
