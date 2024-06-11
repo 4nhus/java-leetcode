13d8821e74bdf0bb82a630e3899bfeec1ebad99f
anh.nguyen0429 <121279445+4nhus@users.noreply.github.com>
2024-06-11 18:12:04 +1000

    Complete Blind75 Java heaps

--- src/Heaps.java
+++ src/Heaps.java
@@ -1,12 +1,19 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.stream.Collectors;
 
 public class Heaps {
     public static List<Point> kClosest(Point[] points, int k) {
         Queue<Point> heap = new PriorityQueue<>((point1, point2) -> point2.distance() - point1.distance());
 
-        for (int i = 0; i < k; i++) {
-            heap.add(points[i]);
-        }
+        heap.addAll(Arrays.asList(points).subList(0, k));
 
         for (int i = k; i < points.length; i++) {
             if (points[i].distance() < heap.peek().distance()) {
@@ -18,22 +25,26 @@ public class Heaps {
         return new ArrayList<>(heap);
     }
 
-    public class Point {
-        int x;
-        int y;
-
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
+    public static List<Integer> topKFrequent(int[] arr, int k) {
+        Map<Integer, Integer> frequency = new HashMap<>();
+        for (int num : arr) {
+            frequency.put(num, frequency.getOrDefault(num, 0) + 1);
         }
 
-        public int distance() {
-            // Ignore square root
-            return x * x + y * y;
+        // Each array contains a number and its frequency
+        PriorityQueue<Map.Entry<Integer, Integer>> topK = new PriorityQueue<>(Comparator.comparingInt(Map.Entry::getValue));
+        for (Map.Entry<Integer, Integer> entry : frequency.entrySet()) {
+            topK.add(entry);
+
+            if (topK.size() > k) {
+                topK.poll();
+            }
         }
+
+        return topK.stream().map(Map.Entry::getKey).collect(Collectors.toList());
     }
 
-    class MedianOfStream {
+    static class MedianOfStream {
         Queue<Integer> topHalf = new PriorityQueue<>();
         Queue<Integer> bottomHalf = new PriorityQueue<>(Collections.reverseOrder());
 
@@ -59,6 +70,21 @@ public class Heaps {
             } else {
                 return bottomHalf.peek();
             }
+        }
+    }
+
+    public static class Point {
+        int x;
+        int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int distance() {
+            // Ignore square root
+            return x * x + y * y;
         }
     }
 }
